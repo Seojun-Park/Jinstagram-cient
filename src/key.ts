4716f507d533a5c3d6374f0dd052a484c8935f39
SeoJun Park <jinchul112@gmail.com>
2021-01-19 01:37:33 +0100

    up

---
+++ src/key.ts
@@ -0,0 +1,14 @@
+export const config = {
+  apiKey: "AIzaSyANPA73ldSYuvfCD7vAxt_1IMIFgSUHSC0",
+  authDomain: "jinsta-storage.firebaseapp.com",
+  databaseURL: "https://jinsta-storage.firebaseio.com",
+  projectId: "jinsta-storage",
+  storageBucket: "jinsta-storage.appspot.com",
+  messagingSenderId: "374149106517",
+  appId: "1:374149106517:web:aa23e6a0e3ce4014fdbddc",
+  measurementId: "G-9HXYJBLQW2"
+};
+
+export const googleApi = {
+  key: "AIzaSyA4NwaePp5gXgNcXDnCejP2vrfKz8mZCzs"
+};
